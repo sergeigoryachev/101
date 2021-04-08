7c07d00f736c0c1af46ac55f55b34215054765f8
sergeigoryachev <77326866+sergeigoryachev@users.noreply.github.com>
2021-04-08 10:07:45 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import os
 
-element_list = os.listdir(".\Documents\expedia-hotel-recommendations")
+element_list = os.listdir()
 #print("List of files in current dir:", element_list)
 
 for i in range(len(element_list)):
     if element_list[i].endswith(".csv"):
         df = pd.read_csv(f".\Documents\expedia-hotel-recommendations\{element_list[i]}")
-        df.to_parquet(".\\Documents\\" + element_list[i] + ".parquet", engine="auto", compression=None)
+        df.to_parquet("/user/hadoop/HW1/" + element_list[i] + ".parquet", engine="auto", compression=None)
     #df_parquet = pd.read_parquet(".\\Documents\\" + element_list[i] + ".parquet")
