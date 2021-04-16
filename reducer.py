c62a19e3cd8627966bf64a008ed694fa56dcc289
sergeigoryachev <77326866+sergeigoryachev@users.noreply.github.com>
2021-04-16 19:37:29 +0300

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import pandas as pd
-import os
 import sys
 
-for item in sys.stdin:
+for item in sys.stdin.split():
    if item.endswith(".csv"):
         df = pd.read_csv(f"/user/hadoop/HW1/{item}")
         df.to_parquet("/user/hadoop/HW1/" + item.split(".")[0] + ".parquet", engine="auto", compression=None)
+        break
     
