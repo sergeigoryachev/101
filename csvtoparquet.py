13a5798b01f61952d540664a4feb870c659c451d
sergeigoryachev <77326866+sergeigoryachev@users.noreply.github.com>
2021-04-18 18:35:33 +0300

    Update csvtoparquet.py

--- csvtoparquet.py
+++ csvtoparquet.py
@@ -1,1 +1,8 @@
+import pandas as pd
 import sys
+
+for item in sys.stdin:
+    if item.endswith(".csv"):
+        df = pd.read_csv("./" + item)
+        df.to_parquet("./" + item.split(".")[0] + ".parquet", engine="auto", compression=None)
+        break
